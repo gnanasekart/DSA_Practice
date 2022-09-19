9f24dd41129a4838ad9cbbd6a1050a039b6c8cc6
t.gnanasekar <gnanasekar69@hotmail.com>
2022-09-19 19:41:58 +0530

    tree

--- src/test/java/Tree/BinaryTree/BinaryTree2.java
+++ src/test/java/Tree/BinaryTree/BinaryTree2.java
@@ -15,6 +15,7 @@ public class BinaryTree2 {
         else return false;
     }
 
+    //insert
     void insert(String value) {
         if (!isFull()) {
             arr[lastUsedIndex + 1] = value;
@@ -22,4 +23,88 @@ public class BinaryTree2 {
             lastUsedIndex++;
         } else System.out.println("Array Full");
     }
+
+    //time comp O(n)
+    //space comp O(n)
+    //preOrder traversal
+    void preorder(int index) {
+        if (index > lastUsedIndex) {
+            return;
+        }
+        System.out.print(arr[index] + " ");
+        preorder(index * 2);//---> n/2
+        preorder(index * 2 + 1);//--->n/2
+    }
+
+    //time comp O(n)
+    //space comp O(n)
+    //inOrder traversal
+    void inOrder(int index) {
+        if (index > lastUsedIndex) {
+            return;
+        }
+        inOrder(index * 2);//---> n/2
+        System.out.print(arr[index] + " ");
+        inOrder(index * 2 + 1);//--->n/2
+    }
+
+    //time comp O(n)
+    //space comp O(n)
+    //postOrder traversal
+    void postOrder(int index) {
+        if (index > lastUsedIndex) {
+            return;
+        }
+        postOrder(index * 2);//---> n/2
+        postOrder(index * 2 + 1);//--->n/2
+        System.out.print(arr[index] + " ");
+    }
+
+    void levelOrdertraversal(){
+        for (int i = 1; i <= lastUsedIndex; i++) {//---> O(n)
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    //time comp O(n)
+    //space comp O(1)
+    // Search method
+    public int search(String value) {
+        for (int i = 1; i<=lastUsedIndex; i++) {
+            if (arr[i] == value) {
+                System.out.println(value+" exists at the location: " + i);
+                return i;
+            }
+        }
+        System.out.println("The value does not exist in BT");
+        return -1;
+    }
+
+    //time comp O(n)
+    //space comp O(1)
+    // Delete Method
+    public void delete(String value) {
+        int location = search(value);
+        if (location == -1) {
+            return;
+        } else {
+            arr[location] = arr[lastUsedIndex];
+            lastUsedIndex--;
+            System.out.println("The node successfully deleted");
+        }
+    }
+
+    // Delete BT
+
+    public void deleteBT() {
+        try {
+            arr = null;
+            System.out.println("The BT has been successfully deleted");
+
+        } catch (Exception e){
+            System.out.println("There was an error deleting the tree");
+        }
+    }
+
+
 }
--- src/test/java/Tree/BinaryTree/BinaryTreeArray.java
+++ src/test/java/Tree/BinaryTree/BinaryTreeArray.java
@@ -5,14 +5,18 @@ import org.testng.annotations.Test;
 public class BinaryTreeArray {
     @Test
     public void example() {
-        BinaryTree2 newNode = new BinaryTree2(5);
+        BinaryTree2 newNode = new BinaryTree2(9);
         newNode.insert("N1");
         newNode.insert("N2");
         newNode.insert("N3");
         newNode.insert("N4");
         newNode.insert("N5");
         newNode.insert("N6");
+        newNode.insert("N7");
+        newNode.insert("N8");
+        newNode.insert("N9");
 
+        newNode.delete("N7");
     }
 
 }
