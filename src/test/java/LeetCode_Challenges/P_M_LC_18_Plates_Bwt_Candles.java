766c8b21e968a3a7448a811fc125f9e4e92c9d12
t.gnanasekar <gnanasekar69@hotmail.com>
2022-10-10 22:33:53 +0530

    Daily LeetCode Pratice

---
+++ src/test/java/LeetCode_Challenges/P_M_LC_18_Plates_Bwt_Candles.java
@@ -0,0 +1,19 @@
+package LeetCode_Challenges;
+
+import org.testng.annotations.Test;
+
+public class P_M_LC_18_Plates_Bwt_Candles {
+
+    //https://leetcode.com/problems/plates-between-candles/
+
+    @Test
+    public void example1(){
+        int[] arr = {0,1,2,3,4,5,6,7,8,9};
+        int[] arr1 = new int[arr.length];
+        for(int i=0; i<arr.length; i++){
+            arr1[i]=arr[i];
+        }
+
+
+    }
+}
