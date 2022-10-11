6bc0dc6f58c3b56f18e022ca0b32751f9cdb05b9
t.gnanasekar <gnanasekar69@hotmail.com>
2022-10-11 18:35:56 +0530

    Daily LeetCode Pratice

--- src/test/java/LeetCode_Challenges/P_M_LC_18_Plates_Bwt_Candles.java
+++ src/test/java/LeetCode_Challenges/P_M_LC_18_Plates_Bwt_Candles.java
@@ -1,5 +1,6 @@
 package LeetCode_Challenges;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class P_M_LC_18_Plates_Bwt_Candles {
@@ -8,12 +9,22 @@ public class P_M_LC_18_Plates_Bwt_Candles {
 
     @Test
     public void example1(){
-        int[] arr = {0,1,2,3,4,5,6,7,8,9};
-        int[] arr1 = new int[arr.length];
-        for(int i=0; i<arr.length; i++){
-            arr1[i]=arr[i];
+        String[] arr={"**|**|***|"};
+        int[][] query={{2,5}, {5,9}};
+        Assert.assertEquals(findPlates(arr, query), new int[] {2,3});
         }
 
 
+    @Test
+    public void example2(){
+        String[] arr={"***|**|*****|**||**|*"};
+        int[][] query={{1,17}, {4,5}, {14,17}, {5,11}, {15,16}};
+        Assert.assertEquals(findPlates(arr, query), new int[] {9,0,0,0,0});
     }
+
+    private Object[] findPlates(Object[] arr, int[][] query) {
+
+    return arr;
+    }
+
 }
