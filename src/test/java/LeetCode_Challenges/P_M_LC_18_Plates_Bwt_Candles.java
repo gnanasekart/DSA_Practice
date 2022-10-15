2c4e55f9301bbfa7710159b86a0130341d32c99f
t.gnanasekar <gnanasekar69@hotmail.com>
2022-10-15 07:55:53 +0530

    Daily LeetCode Pratice

--- src/test/java/LeetCode_Challenges/P_M_LC_18_Plates_Bwt_Candles.java
+++ src/test/java/LeetCode_Challenges/P_M_LC_18_Plates_Bwt_Candles.java
@@ -5,26 +5,45 @@ import org.testng.annotations.Test;
 
 public class P_M_LC_18_Plates_Bwt_Candles {
 
-    //https://leetcode.com/problems/plates-between-candles/
+    /*https://leetcode.com/problems/plates-between-candles/
+    There is a long table with a line of plates and candles arranged on top of it.
+    You are given a 0-indexed string s consisting of characters '*' and '|' only,
+    where a '*' represents a plate and a '|' represents a candle.
 
+    You are also given a 0-indexed 2D integer array queries where queries[i] = [lefti, righti] denotes
+    the substring s[lefti...righti] (inclusive). For each query, you need to find the number of plates
+    between candles that are in the substring. A plate is considered between candles if there is at least
+    one candle to its left and at least one candle to its right in the substring.
+
+    For example, s = "||**||**|*", and a query [3, 8] denotes the substring "*||**|".
+    The number of plates between candles in this substring is 2, as each of the two plates has at
+    least one candle in the substring to its left and right.
+    Return an integer array answer where answer[i] is the answer to the ith query.
+
+        0  1  2  3  4  5  6  7  8  9
+    s:  *  *  |  *  *  |  *  *  *  |
+
+    query[0] = [2,5] = |  *  *  |
+    query[1] = [5,9] = |  *  *  *  |
+* */
     @Test
     public void example1(){
-        String[] arr={"**|**|***|"};
+        String arr="**|**|***|";
         int[][] query={{2,5}, {5,9}};
         Assert.assertEquals(findPlates(arr, query), new int[] {2,3});
         }
 
-
     @Test
     public void example2(){
-        String[] arr={"***|**|*****|**||**|*"};
+        String arr="***|**|*****|**||**|*";
         int[][] query={{1,17}, {4,5}, {14,17}, {5,11}, {15,16}};
         Assert.assertEquals(findPlates(arr, query), new int[] {9,0,0,0,0});
     }
 
-    private Object[] findPlates(Object[] arr, int[][] query) {
+    private int[] findPlates(String arr, int[][] query) {
+        //|**|
 
-    return arr;
+    return null;
     }
 
 }
