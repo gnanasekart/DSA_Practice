f5cd3c7f555dce7733e07e3c1f0ba654a898cab7
t.gnanasekar <gnanasekar69@hotmail.com>
2022-10-21 17:35:34 +0530

    Daily LeetCode Pratice

---
+++ src/main/java/LeetCode_Daily_Practice/Two_Pointers_String/P_O21_1_Find_First_Palindrome.java
@@ -0,0 +1,98 @@
+package LeetCode_Daily_Practice.Two_Pointers_String;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class P_O21_1_Find_First_Palindrome {
+/*
+    https://leetcode.com/problems/find-first-palindromic-string-in-the-array/
+    Given an array of strings words, return the first palindromic string in the array.
+    If there is no such string, return an empty string "".
+
+    A string is palindromic if it reads the same forward and backward.
+
+            Constraints:
+
+            1 <= words.length <= 100
+            1 <= words[i].length <= 100
+    words[i] consists only of lowercase English letters.
+*/
+
+    @Test
+    public void example1(){
+        String[] words={"abc","car","ada","racecar","cool"};
+        Assert.assertEquals(palindromic(words), "ada");
+    }
+
+    @Test
+    public void example2(){
+        String[] words={"notapalindrome","racecar"};
+        Assert.assertEquals(palindromic(words), "racecar");
+    }
+
+    @Test
+    public void example3(){
+        String[] words={"def","ghi"};
+        Assert.assertEquals(palindromic(words), "");
+    }
+
+    @Test
+    public void example4(){
+        String[] words={"d", "dd", "ee"};
+        Assert.assertEquals(palindromic(words), "d");
+    }
+
+    @Test
+    public void example5(){
+        String[] words={"dd"};
+        Assert.assertEquals(palindromic(words), "dd");
+    }
+
+    @Test
+    public void example6(){
+        String[] words={"knywrurkwbrtpalvuuzbczcwzpdqibcwwyflwiddixemsrwblupyerjgvcpavdjfhmujitcsmdbvhxw","ovkeowhqvwlndtpxdnimgietvjsqydbuuwmxkfxxgn","damomwtjugmsrfyvytaheg","bngqatscosdakdwjz","jwzcowuantgqlzjrzgpapcugxvviltrhmcwijtpqapmxjfcilrsmsbeffphcxmaozlczncoxxjmuqijhidxqinhywrtah","ujvoejixvaioikkwzxgtmkchckrigfejjpheiiehpjjefgirkchckmtgxzwkkioiavxijeovju","kacjvcouuigbhydrryaperxvjetwsycmnlkxujaqngjhhotqskclquklxsozfryfxwiksstmropcdvhgsnosgvltqo","qrbsdxxolwzmyltproznfgyydxkkejwdiwpvfzvjoxqvwguoerhclytzvolymbxummwsoqtttyttik","tkekt","esrshrlfoihhjrouleucwpeubygivoatrfraphgwpvtkanwu","kwzrfelynncvsrwvaukiinhjdydmlimggjldhflygemzwnjizzlsuqwahsufwmwhfjkjpngdfsudyavtogoaqzknpew","sdgpcnvsbzxhyjt"};
+        Assert.assertEquals(palindromic(words), "ujvoejixvaioikkwzxgtmkchckrigfejjpheiiehpjjefgirkchckmtgxzwkkioiavxijeovju");
+    }
+/*
+1. Check the condition for empty array
+2. Traversal every word from words array
+3. check the particular word is palidrome or not
+4. if palindromic found then return word
+5. else return emtry string
+*/
+    private String palindromic(String[] words){
+
+        if(words.length <1 ) return "";
+        int i=0;
+        String word = null;
+        while(i<words.length) {
+            if (words[i].length() < 2)
+                return words[i];
+            else {
+                word = words[i++];
+                String revWord = reverseWord(word);
+                if (revWord.equals(word))
+                    return word;
+            }
+        }
+        return "";
+    }
+
+    public String reverseWord(String word){
+        char[] ch = word.trim().toCharArray();
+        char c;
+        int left=0, right=word.length()-1;
+        while(left<right){//=>O(n)
+            if(ch[left]>=95 && ch[right]<=122){
+                c=ch[left];
+                ch[left++]=ch[right];
+                ch[right--]=c;
+            }
+        }
+        return String.valueOf(ch);
+    }
+}
---
+++ src/main/java/LeetCode_Daily_Practice/Two_Pointers_String/P_O21_2_Vovel_Reverse.java
@@ -0,0 +1,83 @@
+package LeetCode_Daily_Practice.Two_Pointers_String;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class P_O21_2_Vovel_Reverse {
+    /*
+    leetcode.com/problems/reverse-vowels-of-a-string/
+
+Given a string s, reverse only all the vowels in the string and return it.
+
+The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+Constraints:
+
+1 <= s.length <= 3 * 105
+s consist of printable ASCII characters
+  */
+
+@Test
+public void example1(){
+String words="hello";
+Assert.assertEquals(reverseVovel(words), "holle");
+}
+
+@Test
+public void example2(){
+String words="leetcode";
+Assert.assertEquals(reverseVovel(words), "leotcede");
+}
+
+@Test
+public void example3(){
+String words="d";
+Assert.assertEquals(reverseVovel(words), "d");
+}
+
+@Test
+public void example4(){
+String words="#$%^e*(a";
+Assert.assertEquals(reverseVovel(words), "#$%^a*(e");
+}
+
+@Test
+public void example5(){
+String words="aeiouAEIOU";
+Assert.assertEquals(reverseVovel(words), "UOIEAuoiea");
+}
+/*
+1. convert the string to char array
+2. consider two pointer from both the end
+3. if both the end matches with vovel then increment both
+4. if left != right then left increment
+5. else right increment
+6. then return the char ch to string
+*/
+private String reverseVovel(String word){
+
+if(word.length()<2) return word;
+
+char[] ch = word.toCharArray();
+int start=0, end=word.length()-1;
+
+ArrayList<Character> list = new ArrayList(Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
+char c;
+
+while(start<end){
+    if((list.contains(ch[start])) && (list.contains(ch[end]))){
+        c=ch[start];
+        ch[start++]=ch[end];
+        ch[end--]=c;
+    }else if(!(list.contains(ch[start])) && ((list.contains(ch[end])))){
+        start++;
+    }else
+        end--;
+}
+
+return String.valueOf(ch);
+}
+
+}
