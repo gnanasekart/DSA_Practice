3cb184d9adfe8228acbe649ee2ec45588bc7bee1
t.gnanasekar <gnanasekar69@hotmail.com>
2023-03-26 20:50:10 +0530

    daily practice

---
+++ src/main/java/LeetCode_Daily_Practice/IV/Chatting_Occurance.java
@@ -0,0 +1,21 @@
+package LeetCode_Daily_Practice.IV;
+
+public class Chatting_Occurance {
+    /*
+    Process a large log file with data from a chat app to find who are the most chatty users.
+    Metric to use for chattiness is the number of words typed by a user. File format:
+
+Preamble<username>chat message
+
+[20190405][11:23pm]<johndoe>Hello World
+[20190405][11:24pm]<janedoe>Hello World Bank
+[20190405][11:24pm]Some Data<johndoe>Hello World Bank Open
+
+Preamble can be ignored. Words always have one space in between them. Line ends with a newline.
+
+Millions of total users U. Find top N users with N ~100-1000.
+
+Write a method that takes a filename and the value of N and returns the top N users
+along with the word counts in descending order.
+     */
+}
